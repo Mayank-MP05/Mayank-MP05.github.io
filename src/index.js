7c7f9042727b900a1c2ecc72a45e620ca70d6a41
Mayank-MP05 <mayank5pande@gmail.com>
2021-06-04 11:30:34 +0530

    Files Added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import ReactGA from "react-ga";
+ReactGA.initialize("274707430"); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("root"));
