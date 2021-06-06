832a4240c89c95971e8ddea9cca35d79109bcfa0
Mayank-MP05 <mayank5pande@gmail.com>
2021-06-06 12:31:14 +0530

    Deploy Two

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import ReactGA from "react-ga";
-ReactGA.initialize("274707430"); // add your tracking id here.
+ReactGA.initialize("G-7WZ415PLJ6"); 
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("root"));
