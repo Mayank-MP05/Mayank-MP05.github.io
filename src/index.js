3d213648344d684a026961a3482c4f29468a820f
Mayank-MP05 <mayank5pande@gmail.com>
2021-06-11 08:11:42 +0530

    Google Analytics Fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import ReactGA from "react-ga";
-ReactGA.initialize("G-7WZ415PLJ6"); 
+ReactGA.initialize("UA-199287147-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById("root"));
