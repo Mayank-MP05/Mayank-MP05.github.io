ed17c0af2ae3c18e8c22c3cc22ff4f59fda6468e
Mayank-MP05 <mayank5pande@gmail.com>
2023-03-15 12:55:37 +0530

    bugfix || about us and highlights strings added data

---
+++ data/about-me-json.js
@@ -0,0 +1,8 @@
+const AboutMeDB = [
+    `Loves JavaScript❤️`,
+    `Interested in Full Stack Developement and Startups`,
+    `Worked mostly on Web Dev and Machine Learning Projects`,
+    `Hobbies - Coding, Video Editing, Memes`
+]
+
+export default AboutMeDB
