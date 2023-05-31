58ec1d7ba4ca22fe73481814601bdade72781747
Mayank-MP05 <mayank5pande@gmail.com>
2023-05-31 12:34:18 +0530

    SEO || seo related changes added in head tag using next js `<Head>` tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,52 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <meta charset="utf-8" />
+                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+
+                <meta property="og:type" content="website" />
+
+                <meta
+                    property="og:title"
+                    content="Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software Development Portfolio"
+                />
+
+                <meta
+                    property="og:description"
+                    content="Software developer with 2+ years of experience working in a fast-paced startups. Having a strong background in full-stack development, and is proficient in a variety of programming languages and technologies like Javascript, Node, Express, MySQL, MongoDB, React, Next JS, AWS, EC2, S3, Codepipeline, Codebuild, Lambda."
+                />
+
+                <meta
+                    property="og:image"
+                    content="https://media.licdn.com/dms/image/C4D03AQEXUmMi3jttZQ/profile-displayphoto-shrink_800_800/0/1659158229535?e=1691020800&v=beta&t=aISqwp9HJWnJ2qbADzgtKfAd6hmtS5CB6fhmt0sMeKA"
+                />
+                <meta property="og:locale" content="en_IN" />
+
+                <meta property="og:url" content="https://mayank5pande.com/" />
+
+                <meta
+                    property="og:site_name"
+                    content="Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software
+Development Portfolio"
+                />
+
+                <meta
+                    name="description"
+                    content="Software developer with 2+ years of experience working in a fast-paced startups. Having a strong background in full-stack development, and is proficient in a variety of programming languages and technologies like Javascript, Node, Express, MySQL, MongoDB, React, Next JS, AWS, EC2, S3, Codepipeline, Codebuild, Lambda. "
+                />
+                <meta name="robots" content="index, follow" />
+                <link rel="icon" href="https://mayank-mp05.github.io/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="theme-color" content="#000000" />
+                <link rel="apple-touch-icon" href="https://mayank-mp05.github.io/favicon.ico" />
+
+                <link rel="canonical" href="https://mayank5pande.com" />
+                <title>
+                    Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software
+                    Development Portfolio
+                </title>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
